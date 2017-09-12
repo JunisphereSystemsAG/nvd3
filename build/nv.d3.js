cb08a860caa6be95433cda5e588ac59d35a85e32
Matthias Balmer <mbalmer@smss.ch>
2017-09-12 14:21:24 +0200

    New version

--- build/nv.d3.js
+++ build/nv.d3.js
@@ -1,4 +1,4 @@
-/* nvd3 version 1.8.5-dev (https://github.com/novus/nvd3) 2017-09-05 */
+/* nvd3 version 1.8.5-dev (https://github.com/novus/nvd3) 2017-09-12 */
 (function(){
 
 // set up main nv object
@@ -9991,26 +9991,36 @@ nv.models.multiChart = function() {
 
             if (dataBars1.length) {
                 d3.transition(bars1Wrap).call(bars1);
-                rbcOffset = bars1.rangeBandCentreOffset();
-                groupSpacing = bars1.groupSpacing();
+                if (dataBars1.some(function(d){return !d.disabled})){
+                    rbcOffset = bars1.rangeBandCentreOffset();
+                    groupSpacing = bars1.groupSpacing();
+                }
             }
             if (dataBars2.length) {
                 d3.transition(bars2Wrap).call(bars2);
-                rbcOffset = bars2.rangeBandCentreOffset();
-                groupSpacing = bars2.groupSpacing();
+                if(dataBars2.some(function(d){return !d.disabled})){
+                    rbcOffset = bars2.rangeBandCentreOffset();
+                    groupSpacing = bars2.groupSpacing();
+                }
             }
 
             if (dataLines1.length) {
                 if(rbcOffset > 0){
-                  lines1.padData(true);
-                  lines1.padDataOuter(groupSpacing);
+                    lines1.padData(true);
+                    lines1.padDataOuter(groupSpacing);
+                } else{
+                    lines1.padData(false);
+                    lines1.padDataOuter(0);
                 }
                 d3.transition(lines1Wrap).call(lines1);
             }
             if (dataLines2.length) {
                 if(rbcOffset > 0){
-                  lines2.padData(true);
-                  lines2.padDataOuter(groupSpacing);
+                    lines2.padData(true);
+                    lines2.padDataOuter(groupSpacing);
+                } else {
+                    lines2.padData(false);
+                    lines2.padDataOuter(0);
                 }
                 d3.transition(lines2Wrap).call(lines2);
             }
@@ -10023,10 +10033,14 @@ nv.models.multiChart = function() {
                 ._ticks( nv.utils.calcTicksX(availableWidth/100, data) )
                 .tickSize(-availableHeight, 0);
 
+            var xAxisScale = ((availableWidth - rbcOffset*2)/availableWidth);
+
+            xAxis.fontSize(12 / xAxisScale);
+
             g.select('.nv-x.nv-axis')
                 .attr('transform',
-                       'translate(' + rbcOffset + ', ' + availableHeight + ') ' +
-                       'scale(' + ((availableWidth - rbcOffset*2)/availableWidth) + ', 1)'
+                       'translate(' + rbcOffset + ', ' + (availableHeight + 3 / xAxisScale) + ') ' +
+                       'scale(' + xAxisScale + ', ' + xAxisScale + ')'
                 )
 
             d3.transition(g.select('.nv-x.nv-axis'))
